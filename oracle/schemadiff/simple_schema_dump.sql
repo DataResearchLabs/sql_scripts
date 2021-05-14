3d38fe69c38d03ba7396e8fe8089dd0bd3e5b3ec
Matt Pierce <76134461+drlmpierce@users.noreply.github.com>
2021-05-13 21:33:06 -0700

    Update simple_schema_dump.sql

--- oracle/schemadiff/simple_schema_dump.sql
+++ oracle/schemadiff/simple_schema_dump.sql
@@ -10,7 +10,7 @@
 ------------------------------------------------------------------------------------
 -- Platform:          Oracle 11g or later
 -- Author:            DataResearchLabs
--- GitHub:            https://www.github.com/DataResearchLabs
+-- GitHub:            https://github.com/DataResearchLabs/sql_scripts
 -- YouTube Tutorials: https://www.youtube.com/channel/UCQciXv3xaBykeUFc04GxSXA
 ----------------------------------------------------------------------------------
 
