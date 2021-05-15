ccb12f3d6aa4840d80ac5ce046bd1d523b4d8cd8
Matt Pierce <76134461+drlmpierce@users.noreply.github.com>
2021-05-15 15:01:09 -0700

    Added PKey, FKey and UKey as a column IS_KEYS

--- oracle/data_dictionary/data_dict_dump.sql
+++ oracle/data_dictionary/data_dict_dump.sql
@@ -20,37 +20,69 @@ AS (
   FROM dual
 )
 
+, meta_data
+AS (
+	SELECT
+	  atc.owner       AS SCHEMA_NM
+	, atc.table_name  AS TABLE_NM
+	, atc.column_id   AS ORD_POS
+	, atc.column_name AS COLUMN_NM 
+	, (atc.data_type ||
+	    decode(atc.data_type,
+	      'NUMBER',
+	         decode(atc.data_precision, null, '',
+	          '(' || to_char(atc.data_precision) || decode(atc.data_scale,null,'',0,'',',' || to_char(atc.data_scale) )
+	              || ')' ),
+	      'FLOAT', '(' || to_char(atc.data_precision) || ')',
+	      'VARCHAR2', '(' || to_char(atc.data_length) || ')',
+	      'NVARCHAR2', '(' || to_char(atc.data_length) || ')',
+	      'VARCHAR', '(' || to_char(atc.data_length) || ')',
+	      'CHAR', '(' || to_char(atc.data_length) || ')',
+	      'RAW', '(' || to_char(atc.data_length) || ')',
+	      'MLSLABEL',decode(atc.data_length,null,'',0,'','(' || to_char(atc.data_length) || ')'),
+	      '')
+	  )                 AS DATA_TYP
+	, CASE WHEN atc.nullable = 'Y' THEN 'NULL' ELSE 'NOT NULL' END AS NULLABLE
+	, dcc.comments  AS DESCRIPTION
+	FROM       all_tab_columns  atc
+	INNER JOIN all_col_comments dcc ON atc.owner = dcc.owner AND atc.table_name = dcc.table_name AND atc.column_name = dcc.column_name
+	INNER JOIN all_tab_comments t   ON t.OWNER = atc.owner   AND t.TABLE_NAME = atc.table_name
+	WHERE atc.owner = (SELECT vars.v_SchemaName FROM vars)
+	  AND (    (t.TABLE_TYPE = 'TABLE')
+	       OR  ((SELECT v_TablesOnly FROM vars) = 'NO')  
+	      )
+)
 
-SELECT
-  atc.owner       AS SCHEMA_NM
-, atc.table_name  AS TBL_NM
-, atc.column_id   AS COL_ORD_POS
-, atc.column_name AS COL_NM
-, (atc.data_type ||
-    decode(atc.data_type,
-      'NUMBER',
-         /* scale is not null or both scale and precision are null */
-         decode(atc.data_precision, null, '',
-          '(' || to_char(atc.data_precision) || decode(atc.data_scale,null,'',0,'',',' || to_char(atc.data_scale) )
-              || ')' ),
-      'FLOAT', '(' || to_char(atc.data_precision) || ')',
-         /* Float: Scale is Null & Precision is not Null */
-      'VARCHAR2', '(' || to_char(atc.data_length) || ')',
-      'NVARCHAR2', '(' || to_char(atc.data_length) || ')',
-      'VARCHAR', '(' || to_char(atc.data_length) || ')',
-      'CHAR', '(' || to_char(atc.data_length) || ')',
-      'RAW', '(' || to_char(atc.data_length) || ')',
-      'MLSLABEL',decode(atc.data_length,null,'',0,'','(' || to_char(atc.data_length) || ')'),
-      '')
-  )                 AS COL_DATA_TYP
-, atc.nullable      AS COL_IS_NULLABLE
-, atc.data_default  AS COL_DFLT_VAL
-, dcc.comments      AS COL_DESCRIP
-FROM       all_tab_columns  atc
-INNER JOIN all_col_comments dcc ON atc.owner = dcc.owner AND atc.table_name = dcc.table_name AND atc.column_name = dcc.column_name
-INNER JOIN all_tab_comments t   ON t.OWNER = atc.owner   AND t.TABLE_NAME = atc.table_name
-WHERE atc.owner = (SELECT vars.v_SchemaName FROM vars)
-  AND (    (t.TABLE_TYPE = 'TABLE')
-       OR  ((SELECT v_TablesOnly FROM vars) = 'NO')  
-      )
-ORDER BY atc.owner, atc.table_name, atc.column_id
+
+, meta_for_keys
+AS (
+	SELECT SCHEMA_NM, TABLE_NM, COLUMN_NM
+	, LISTAGG(IS_KEY, ', ') 
+	  WITHIN GROUP(ORDER BY IS_KEY DESC) AS IS_KEY
+	FROM (
+	  SELECT cons.owner    AS SCHEMA_NM
+	  , cols.table_name    AS TABLE_NM
+	  , cols.column_name   AS COLUMN_NM
+	  , CASE WHEN cons.constraint_type = 'P' THEN 'PK'
+	         WHEN cons.constraint_type = 'U' THEN 'UK'
+	         WHEN cons.constraint_type = 'R' THEN 'FK'
+	    END                AS IS_KEY
+	  FROM all_constraints cons 
+	    INNER JOIN all_cons_columns cols ON cons.constraint_name = cols.constraint_name AND cons.owner = cols.owner 
+	  WHERE cons.owner = (SELECT vars.v_SchemaName FROM vars)
+	    AND cons.table_name IN(SELECT DISTINCT TABLE_NM FROM meta_data)
+	    AND cons.constraint_type IN('P','R','U') 
+	  GROUP BY cons.owner, cols.table_name, cols.column_name, cons.constraint_type
+   ) t
+   GROUP BY SCHEMA_NM, TABLE_NM, COLUMN_NM
+)
+
+
+SELECT md.SCHEMA_NM, md.TABLE_NM
+, COALESCE(pk.IS_KEY, ' ') AS KEYS
+, md.ORD_POS AS ORD
+, md.COLUMN_NM, md.DATA_TYP, md.NULLABLE, md.DESCRIPTION
+FROM      meta_data     md
+LEFT JOIN meta_for_keys pk ON pk.SCHEMA_NM = md.SCHEMA_NM AND pk.TABLE_NM = md.TABLE_NM AND pk.COLUMN_NM = md.COLUMN_NM
+ORDER BY md.SCHEMA_NM, md.TABLE_NM, md.ORD_POS
+
