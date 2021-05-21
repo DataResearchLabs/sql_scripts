2bbf36c81814bf64102982e92610c1a19ed70e71
Matt Pierce <76134461+drlmpierce@users.noreply.github.com>
2021-05-20 20:20:31 -0700

    Refactor to Greenplum from PostgreSQL

--- greenplum/data_dictionary/data_dict_dump.sql
+++ greenplum/data_dictionary/data_dict_dump.sql
@@ -3,7 +3,7 @@
 -- This SQL script will dump table, column, key, and description design related 
 -- metadata so that you can copy-paste or export to Excel as a Data Dictionary.  
 ------------------------------------------------------------------------------------
--- Platform:          PostgreSQL
+-- Platform:          Greenplum
 -- Author:            DataResearchLabs
 -- GitHub:            https://github.com/DataResearchLabs/sql_scripts
 -- YouTube Tutorials: https://www.youtube.com/channel/UCQciXv3xaBykeUFc04GxSXA
@@ -11,8 +11,8 @@
 WITH vars
 AS (
   SELECT 
-    'public'     AS v_SchemaName  -- Set to the schema whose tables you want in the Data Dictionary
-  , 'NO'         AS v_TablesOnly  -- YES=Limit To Tables only; NO=Include views too 
+    'acq_cms' :: CHARACTER VARYING(50) AS v_SchemaName  -- <<<<<<<<<<<<<<<< Set to the schema you wish to document
+  , 'NO'      :: CHARACTER VARYING(10) AS v_TablesOnly  -- YES=Limit To Tables only; NO=Include views too 
 )
 
 , baseTbl
@@ -38,71 +38,90 @@ AS (
 	  END AS obj_typ
 	, tut.ordinal_position   AS ord_pos
 	, tut.column_name        AS column_nm 
-    , CONCAT(tut.data_type, 
-      CASE WHEN tut.data_type IN('varchar','char')            THEN CONCAT('(', tut.CHARACTER_MAXIMUM_LENGTH, ')')
-	       WHEN tut.data_type IN('date','time')           THEN CONCAT('(3)')
-	       WHEN tut.data_type = 'datetime'                THEN CONCAT('(8)')
-	       WHEN tut.data_type = 'timestamp'               THEN CONCAT('(4)')
-	       WHEN tut.data_type LIKE '%int%'                THEN CONCAT('(', tut.NUMERIC_PRECISION, ')')
-	       WHEN tut.data_type = 'decimal'                 THEN CONCAT('(', tut.NUMERIC_PRECISION, ',', tut.NUMERIC_SCALE, ')')
-	       WHEN tut.CHARACTER_MAXIMUM_LENGTH IS NOT NULL  THEN CONCAT('(', tut.CHARACTER_MAXIMUM_LENGTH, ')')
-		   WHEN tut.DATETIME_PRECISION IS NOT NULL    THEN CONCAT('(', tut.DATETIME_PRECISION, ')')
-	       WHEN tut.NUMERIC_PRECISION IS NOT NULL
-		    AND tut.NUMERIC_SCALE     IS NULL         THEN CONCAT('(', tut.NUMERIC_PRECISION, ')')
-	       WHEN tut.NUMERIC_PRECISION IS NOT NULL
-	        AND tut.NUMERIC_SCALE     IS NOT NULL         THEN CONCAT('(', tut.NUMERIC_PRECISION, ',', tut.NUMERIC_SCALE, ')')
+    , tut.data_type || 
+      CASE WHEN tut.data_type IN('varchar','char')        THEN '(' || tut.CHARACTER_MAXIMUM_LENGTH || ')'
+	         WHEN tut.data_type IN('date','time')           THEN '(3)'
+	         WHEN tut.data_type = 'datetime'                THEN '(8)'
+  	       WHEN tut.data_type = 'timestamp'               THEN '(4)'
+	         WHEN tut.data_type LIKE '%int%'                THEN '(' || tut.NUMERIC_PRECISION || ')'
+	         WHEN tut.data_type = 'decimal'                 THEN '(' || tut.NUMERIC_PRECISION || ',' || tut.NUMERIC_SCALE || ')'
+	         WHEN tut.CHARACTER_MAXIMUM_LENGTH IS NOT NULL  THEN '(' || tut.CHARACTER_MAXIMUM_LENGTH || ')'
+           WHEN tut.DATETIME_PRECISION IS NOT NULL        THEN '(' || tut.DATETIME_PRECISION || ')'
+  	       WHEN tut.NUMERIC_PRECISION IS NOT NULL
+	          AND tut.NUMERIC_SCALE     IS NULL             THEN '(' || tut.NUMERIC_PRECISION || ')'
+	         WHEN tut.NUMERIC_PRECISION IS NOT NULL
+	          AND tut.NUMERIC_SCALE     IS NOT NULL         THEN '(' || tut.NUMERIC_PRECISION || ',' || tut.NUMERIC_SCALE || ')'
 		   ELSE ''
-    END ) AS data_typ 
+    END AS data_typ 
   , CASE WHEN tut.IS_NULLABLE = 'YES' THEN 'NULL' ELSE 'NOT NULL' END AS nullable
   FROM       INFORMATION_SCHEMA.COLUMNS tut
   INNER JOIN baseTbl                    bt  ON bt.table_catalog = tut.TABLE_CATALOG AND bt.table_name = tut.table_name
 )
 
+
 , meta_for_keys
 AS (
-  SELECT schema_nm, table_nm, column_nm
-  , STRING_AGG(is_key, ',' ORDER BY is_key) AS is_key
-  FROM (
-    SELECT cons.TABLE_SCHEMA AS schema_nm
-    , cons.TABLE_NAME        AS table_nm
-    , kcu.COLUMN_NAME        AS column_nm
-    , CASE WHEN cons.constraint_type = 'PRIMARY KEY' THEN 'PK'
-           WHEN cons.constraint_type = 'UNIQUE'      THEN 'UK'
-           WHEN cons.constraint_type = 'FOREIGN KEY' THEN 'FK'
-      END AS is_key
-    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS      cons 
-    INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu 
-       ON cons.TABLE_SCHEMA = kcu.TABLE_SCHEMA  
-      AND cons.TABLE_NAME = kcu.TABLE_NAME
-  	  AND cons.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME
-    WHERE cons.table_schema = (SELECT v_SchemaName FROM vars) 
-      AND cons.table_name IN(SELECT DISTINCT table_name FROM baseTbl)
-      AND cons.constraint_type IN('PRIMARY KEY','FOREIGN KEY','UNIQUE') 
-    GROUP BY cons.TABLE_SCHEMA, cons.TABLE_NAME, kcu.COLUMN_NAME, cons.constraint_type
-  ) t
-  GROUP BY schema_nm, table_nm, column_nm
+  SELECT schema_nm, table_nm
+  , COALESCE(distribution_keys,'DISTRIBUTED RANDOMLY') :: CHARACTER VARYING(255) AS column_nm
+  , 'DK' :: CHARACTER VARYING(10) AS is_key
+  FROM 
+  ( SELECT pgn.nspname AS schema_nm,
+    pgc.relname AS table_nm, 
+    pga.attname AS distribution_keys
+    FROM 
+    ( SELECT gdp.localoid 
+      , CASE WHEN ( Array_upper(gdp.attrnums, 1) > 0 ) THEN Unnest(gdp.attrnums)
+             ELSE NULL 
+        END As attnum 
+      FROM gp_distribution_policy gdp 
+      ORDER BY gdp.localoid 
+    ) AS distrokey 
+    INNER JOIN pg_class          pgc ON distrokey.localoid = pgc.oid 
+    INNER JOIN pg_namespace      pgn ON pgc.relnamespace = pgn.oid 
+    LEFT OUTER JOIN pg_attribute pga ON distrokey.attnum = pga.attnum AND distrokey.localoid = pga.attrelid 
+    WHERE pgn.nspname = (SELECT v_SchemaName FROM vars)
+      AND pgc.relname IN(SELECT DISTINCT table_name FROM baseTbl) 
+    ) AS a 
 )
 
-, col_comm
+, col_descr
 AS (
-	SELECT c.TABLE_SCHEMA AS schema_nm
-	, c.TABLE_NAME        AS table_nm
-	, c.COLUMN_NAME       AS column_nm
-	, pgd.DESCRIPTION     AS column_descr
-	FROM pg_catalog.pg_statio_all_tables   AS st
-	INNER JOIN pg_catalog.PG_DESCRIPTION   AS pgd ON pgd.objoid = st.relid
-	INNER JOIN INFORMATION_SCHEMA.COLUMNS  AS c   ON pgd.objsubid = c.ordinal_position
-	                                             AND c.table_schema = st.schemaname
-	                                             AND c.table_name = st.relname
-	WHERE c.table_schema = (SELECT v_SchemaName FROM vars) 
-	  AND c.table_name IN(SELECT DISTINCT table_name FROM baseTbl)
+  SELECT schema_nm
+  , REPLACE(table_nm, schema_nm || '.', '') AS table_nm
+  , column_nm, column_descr
+  FROM (
+    SELECT 
+      ns.nspname              AS schema_nm
+    , (a.attrelid :: regclass) :: VARCHAR(150) AS table_nm
+    , a.attname               AS column_nm
+    , d.description           AS column_descr
+    FROM      pg_catalog.pg_attribute   a
+    LEFT JOIN pg_catalog.pg_class       c  ON a.attrelid = c.oid
+    LEFT JOIN pg_catalog.pg_namespace   ns ON c.relnamespace = ns.oid
+    LEFT JOIN pg_catalog.pg_description d  ON d.objoid = a.attrelid AND d.objsubid = a.attnum
+    WHERE a.attnum > 0
+      AND NOT a.attisdropped  
+  ) t
+  WHERE schema_nm = (SELECT v_SchemaName FROM vars) 
+    AND REPLACE(table_nm, schema_nm || '.', '') IN(SELECT DISTINCT table_name FROM baseTbl)
 )
 
-SELECT md.SCHEMA_NM, md.TABLE_NM, md.OBJ_TYP
-, md.ORD_POS AS ord
-, COALESCE(pk.is_key, ' ') AS is_key
-, md.COLUMN_NM, md.DATA_TYP, md.NULLABLE, c.column_descr 
-FROM      metadata      md
-LEFT JOIN meta_for_keys pk ON pk.SCHEMA_NM = md.SCHEMA_NM AND pk.TABLE_NM = md.TABLE_NM AND pk.COLUMN_NM = md.COLUMN_NM
-LEFT JOIN col_comm      c  ON c.SCHEMA_NM  = md.SCHEMA_NM AND c.TABLE_NM  = md.TABLE_NM AND c.COLUMN_NM  = md.COLUMN_NM
-ORDER BY md.SCHEMA_NM, md.TABLE_NM, md.ORD_POS
+, combined_data
+AS (
+  SELECT md.schema_nm
+  , md.table_nm
+  , md.obj_typ
+  , md.ord_pos AS ord
+  , COALESCE(dk.is_key, ' ')  AS is_key
+  , md.column_nm
+  , md.data_typ
+  , md.nullable
+  , cd.column_descr 
+  FROM      metadata      md
+  LEFT JOIN col_descr     cd ON cd.schema_nm = md.schema_nm AND cd.table_nm = md.table_nm AND cd.column_nm = md.column_nm
+  LEFT JOIN meta_for_keys dk ON dk.schema_nm = md.schema_nm AND dk.table_nm = md.table_nm AND dk.column_nm = md.column_nm
+)
+
+SELECT *
+FROM combined_data
+ORDER BY schema_nm, table_nm, ord
